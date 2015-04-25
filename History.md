4aaeb8cd2b204b646bb8b884506fc4def55ad0d2
William Guan <blackgun99@gmail.com>
2015-04-25 10:50:36 -0700

    fix the video ads crashs

---
+++ History.md
@@ -0,0 +1,5 @@
+1.3.1 / 2015-04-25
+===================
+ * Fixed the viedo ads crash issues
+
+ 
--- iOS-SDK/MoSDK.EmbeddedFramework/MoSDK.framework/Versions/A/Headers/MoSDK.h
+++ iOS-SDK/MoSDK.EmbeddedFramework/MoSDK.framework/Versions/A/Headers/MoSDK.h
@@ -15,25 +15,39 @@ FOUNDATION_EXPORT double AdsPackageVersionNumber;
 FOUNDATION_EXPORT const unsigned char AdsPackageVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AdsPackage/AdsPackage.h>
-
 @protocol AdsPackageDelegate <NSObject>
-
 @optional
-
 - (void)passLog:(NSString*)paramLog;
-
 @end
 
+//forward declarations
+@class MoSDK;
+
+// MosSDK delegate protocol
+@protocol MoSDKDelegate <NSObject>
+@optional
+// Methods for notification of Ads related events.
+// Notifies about MoSDK received request to show Ad
+-(void)MoSDK_didRequestedAd:(MoSDK*)moSDK;
+// Notifies about MoSDK has displayed Ad
+-(void)MoSDK_didShowAd:(MoSDK*)moSDK;
+// Notifies about MoSDK received user's click on Ad
+-(void)MoSDK_didClickAd:(MoSDK*)moSDK;
+@end
+
+// MoSDK interface
 @interface MoSDK : NSObject
 
+@property(nonatomic, weak) id< MoSDKDelegate > delegate;
 
+// Returns/creates singleton shared instance of MoSDK
 + (MoSDK *)sharedInstance;
-
-+ (void) startWithAppId:(NSString *)appId;
-
-+ (void) showNextAd;
-
+// Returns MoSDK version string
 + (NSString*)versionInfo;
+// Initializes MoSDK with specified credentials
++ (void)startWithAppId:(NSString *)appId;
+// Requests Ad displaying
++ (void)showNextAd;
 
 @end
 
